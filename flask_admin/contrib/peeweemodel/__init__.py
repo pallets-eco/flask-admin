b6614b7d4df4915a2d0f6f367d3031c73614a7a8
Serge S. Koval <serge.koval@gmail.com>
2012-08-26 14:46:10 +0300

    Added peewee and wtfpeewee package check

--- flask_admin/contrib/peeweemodel/__init__.py
+++ flask_admin/contrib/peeweemodel/__init__.py
@@ -1,1 +1,7 @@
+try:
+    import peewee
+    import wtfpeewee
+except ImportError:
+    raise Exception('Please install peewee and wtfpeewee packages in order to use peewee integration')
+
 from .view import ModelView
