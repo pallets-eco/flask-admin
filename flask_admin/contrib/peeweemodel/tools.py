b9c153d3eb18394c370193e04509b436691db86a
Iurii Ogiienko <iurii.ogiienko@gmail.com>
2013-02-03 12:15:09 +0200

    Update flask_admin/contrib/peeweemodel/tools.py
    Peewee allow to create PrimaryKeys like IntegerField(primary_key=True)

--- flask_admin/contrib/peeweemodel/tools.py
+++ flask_admin/contrib/peeweemodel/tools.py
@@ -3,7 +3,7 @@ from peewee import PrimaryKeyField
 
 def get_primary_key(model):
     for n, f in model._meta.get_sorted_fields():
-        if type(f) == PrimaryKeyField:
+        if type(f) == PrimaryKeyField or f.primary_key:
             return n
 
 
