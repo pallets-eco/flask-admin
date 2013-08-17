b55512265f5f69fa5e31227a83018ebd72e1b686
Bruno Rocha <rochacbruno@gmail.com>
2013-08-17 00:06:19 -0300

    removed unused imports

--- flask_admin/contrib/mongoengine/view.py
+++ flask_admin/contrib/mongoengine/view.py
@@ -9,7 +9,6 @@ from flask.ext.admin._compat import iteritems, string_types
 
 import mongoengine
 import gridfs
-from mongoengine.fields import GridFSProxy, ImageGridFsProxy
 from mongoengine.connection import get_db
 from bson.objectid import ObjectId
 
