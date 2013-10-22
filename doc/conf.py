052dacf0256aa75b57b353431cfcff7c441dbd52
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-10-22 19:03:43 +0200

    Replace Flask logo with scroll.

--- doc/conf.py
+++ doc/conf.py
@@ -115,7 +115,7 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = '_static/flask.png'
+#html_logo = '_static/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
