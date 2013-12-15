edd926a45cce94a46d4b4b1d764b18d316c52428
Serge S. Koval <serge.koval+github@gmail.com>
2013-12-15 22:45:26 +0200

    Removed debug print.

--- flask_admin/static/admin/js/filters.js
+++ flask_admin/static/admin/js/filters.js
@@ -11,7 +11,6 @@ var AdminFilters = function(element, filters_element, operations, options, types
     function changeOperation() {
         var $row = $(this).closest('tr');
         var $el = $('.filter-val:input', $row);
-        console.log($el);
         var count = getCount($el.attr('name'));
         $el.attr('name', 'flt' + count + '_' + $(this).val());
         $('button', $root).show();
