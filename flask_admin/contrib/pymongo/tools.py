1b07f2cfcf78477a5bf9cc0a836f9416a7f8dce6
Raz0r <me@raz0r.name>
2014-07-01 18:15:02 +0400

    escape data before being used in query
    dangerous!

--- flask_admin/contrib/pymongo/tools.py
+++ flask_admin/contrib/pymongo/tools.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_like_term(term):
     """
         Parse search term into (operation, term) tuple
@@ -6,8 +8,8 @@ def parse_like_term(term):
             Search term
     """
     if term.startswith('^'):
-        return '^%s' % term[1:]
+        return '^{}'.format(re.escape(term[1:]))
     elif term.startswith('='):
-        return '^%s$' % term[1:]
+        return '^{}$'.format(re.escape(term[1:]))
 
-    return '%s' % term
+    return re.escape(term)
