6e51be65148c9b62a3d04d2ca31cff962231fc8f
Alex Frazer <alexadfrazer@gmail.com>
2014-07-06 17:36:34 -0400

    added a dependency which made it hard to import ModelView

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setup(
     platforms='any',
     install_requires=[
         'Flask>=0.7',
-        'wtforms<2.0'
+        'wtforms<2.0',
+	'python-dateutil'
     ],
     tests_require=[
         'nose>=1.0',
