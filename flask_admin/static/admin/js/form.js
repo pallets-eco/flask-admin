52a9e6afdf1b0af024f2f03e62edf4a59adb7946
boomard <boombard@bitbucket.org>
2014-08-11 13:51:29 +0200

    changed data-date-format for datetime bootstrap3 filters. Solves same issue as #603 for date fields

--- flask_admin/static/admin/js/form.js
+++ flask_admin/static/admin/js/form.js
@@ -116,7 +116,6 @@
                 return true;
             case 'datetimepicker':
                 $el.datetimepicker({
-                  //format: 'YYYY-MM-DD hh:mm:ss'
                 });
                 return true;
             case 'timepicker':
