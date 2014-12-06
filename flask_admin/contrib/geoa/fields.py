f8a26abf3fb1022b17dd667a31a990041c517ea2
Alex Kerney <abk@mac.com>
2014-12-06 18:07:11 -0500

    Dealing with when the srid is not defined

--- flask_admin/contrib/geoa/fields.py
+++ flask_admin/contrib/geoa/fields.py
@@ -41,6 +41,10 @@ class GeoJSONField(JSONField):
         super(GeoJSONField, self).__init__(label, validators, **kwargs)
         self.web_srid = 4326
         self.srid = srid
+        if self.srid is -1:
+        	self.transform_srid = self.web_srid
+        else:
+        	self.transform_srid = self.srid
         self.geometry_type = geometry_type.upper()
         self.session = session
 
@@ -48,11 +52,14 @@ class GeoJSONField(JSONField):
         if self.raw_data:
             return self.raw_data[0]
         if type(self.data) is geoalchemy2.elements.WKBElement:
-            self.data = self.session.scalar(func.ST_AsGeoJson(func.ST_Transform(self.data, self.web_srid)))
+        	if self.srid is -1:
+	        	self.data = self.session.scalar(func.ST_AsGeoJson(self.data))
+	        else:
+	            self.data = self.session.scalar(func.ST_AsGeoJson(func.ST_Transform(self.data, self.web_srid)))
         return super(GeoJSONField, self)._value()
 
     def process_formdata(self, valuelist):
         super(GeoJSONField, self).process_formdata(valuelist)
-        web_shape = self.session.scalar(func.ST_AsText(func.ST_Transform(func.ST_GeomFromText(shape(self.data).wkt, self.web_srid), self.srid)))
+        web_shape = self.session.scalar(func.ST_AsText(func.ST_Transform(func.ST_GeomFromText(shape(self.data).wkt, self.web_srid), self.transform_srid)))
         if type(self.data):
             self.data = 'SRID='+str(self.srid)+';'+str(web_shape)
