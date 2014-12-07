0d565dace70d42d1c9d9ada6dad60b93ff4c54eb
Alex Kerney <abk@mac.com>
2014-12-06 19:09:00 -0500

    Changed the DEFAULT_FORMATTERS to WKBElement instead of Shapely

--- flask_admin/contrib/geoa/typefmt.py
+++ flask_admin/contrib/geoa/typefmt.py
@@ -3,7 +3,8 @@ import json
 from jinja2 import Markup
 from wtforms.widgets import html_params
 from shapely.geometry import mapping
-from shapely.geometry.base import BaseGeometry
+from geoalchemy2.shape import to_shape
+from geoalchemy2.elements import WKBElement
 
 
 def geom_formatter(view, value):
@@ -12,12 +13,12 @@ def geom_formatter(view, value):
         "disabled": "disabled",
         "data-width": 100,
         "data-height": 70,
-        "data-geometry-type": value.geom_type,
+        "data-geometry-type": to_shape(value).geom_type,
         "data-zoom": 15,
     })
-    geojson = json.dumps(mapping(value))
+    geojson = json.dumps(mapping(to_shape(value)))
     return Markup('<textarea %s>%s</textarea>' % (params, geojson))
 
 
 DEFAULT_FORMATTERS = BASE_FORMATTERS.copy()
-DEFAULT_FORMATTERS[BaseGeometry] = geom_formatter
+DEFAULT_FORMATTERS[WKBElement] = geom_formatter
