2918eee54cfc544618e945cc6592ca8cf2949788
Paul Brown <paul90brown@gmail.com>
2015-06-30 21:54:36 -0500

    fix WYSIWYG example for bootstrap3

--- examples/wysiwyg/app.py
+++ examples/wysiwyg/app.py
@@ -18,10 +18,16 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-# Define wtforms widget and field
+''' Define a wtforms widget and field.
+
+    WTForms documentation on custom widgets:
+    http://wtforms.readthedocs.org/en/latest/widgets.html#custom-widgets
+'''
 class CKTextAreaWidget(widgets.TextArea):
     def __call__(self, field, **kwargs):
-        kwargs.setdefault('class_', 'ckeditor')
+        # add WYSIWYG class to existing classes
+        existing_classes = kwargs.pop('class', '') or kwargs.pop('class_', '')
+        kwargs['class'] = u'%s %s' % (existing_classes, "ckeditor")
         return super(CKTextAreaWidget, self).__call__(field, **kwargs)
 
 
