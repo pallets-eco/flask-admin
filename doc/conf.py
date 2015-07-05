c91356ea7ea2aac1e4390a693708ab81d81e8d7b
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2015-07-05 12:08:26 +0200

    Update copyright year.

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'flask-admin'
-copyright = u'2012-2013, Serge S. Koval'
+copyright = u'2012-2015, Serge S. Koval'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
