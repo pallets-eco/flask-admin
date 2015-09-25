3246c15cd33e7eb95d646d04c00516c5781a9022
Chase Seibert <chase.seibert+github@gmail.com>
2015-09-25 13:39:37 -0700

    Make QuerySelectMultipleField fields editable on the list view
    For QuerySelectField and ModelSelectField, a select data-type is used.
    This element does not support select multiple, but the checklist element
    does. The same code for compiling the choices is used.

--- flask_admin/model/widgets.py
+++ flask_admin/model/widgets.py
@@ -141,9 +141,11 @@ class XEditableWidget(object):
         elif subfield.type in ['FloatField', 'DecimalField']:
             kwargs['data-type'] = 'number'
             kwargs['data-step'] = 'any'
-        elif subfield.type in ['QuerySelectField', 'ModelSelectField']:
-            # QuerySelectField and ModelSelectField are for relations
+        elif subfield.type in ['QuerySelectField', 'ModelSelectField', 'QuerySelectMultipleField']:
+            # QuerySelectField, ModelSelectField and QuerySelectMultipleField are for relations
             kwargs['data-type'] = 'select'
+            if subfield.type == 'QuerySelectMultipleField':
+                kwargs['data-type'] = 'checklist'
 
             choices = []
             for choice in subfield:
