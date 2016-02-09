d1a0fcc84c886dc676b1a051b61f02396ef1daf2
Paul Brown <paul90brown@gmail.com>
2016-02-08 19:45:39 -0600

    fix ReferenceField filter validation and support python 3

--- flask_admin/contrib/mongoengine/filters.py
+++ flask_admin/contrib/mongoengine/filters.py
@@ -2,6 +2,7 @@ from flask_admin.babel import lazy_gettext
 from flask_admin.model import filters
 
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from mongoengine.queryset import Q
 
 from .tools import parse_like_term
@@ -248,7 +249,7 @@ class ReferenceObjectIdFilter(BaseMongoEngineFilter):
         try:
             self.clean(value)
             return True
-        except ValueError:
+        except InvalidId:
             return False
 
     def clean(self, value):
