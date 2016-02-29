f06f6f7e7c55b06e2529119a6726ca61988f415c
Serge S. Koval <serge.koval@gmail.com>
2016-02-29 17:03:35 -0500

    Optional url caching for MenuView

--- flask_admin/menu.py
+++ flask_admin/menu.py
@@ -81,13 +81,14 @@ class MenuView(BaseMenu):
     """
         Admin view menu item
     """
-    def __init__(self, name, view=None):
+    def __init__(self, name, view=None, cache=True):
         super(MenuView, self).__init__(name,
                                        class_name=view.menu_class_name,
                                        icon_type=view.menu_icon_type,
                                        icon_value=view.menu_icon_value)
 
         self._view = view
+        self._cache = cache
         self._cached_url = None
 
         view.menu = self
@@ -99,8 +100,12 @@ class MenuView(BaseMenu):
         if self._cached_url:
             return self._cached_url
 
-        self._cached_url = self._view.get_url('%s.%s' % (self._view.endpoint, self._view._default_view))
-        return self._cached_url
+        url = self._view.get_url('%s.%s' % (self._view.endpoint, self._view._default_view))
+
+        if self._cache:
+            self._cached_url = url
+
+        return url
 
     def is_active(self, view):
         if view == self._view:
