pallets-eco/flask-admin

commit 47080aeb1ffee4ea5cfd3254c18209ea9b4a04ae

added support for remote hybrid properties filters.

flask_admin/contrib/sqla/tools.py
@@ -190,7 +190,12 @@ def get_hybrid_properties(model):
 
 
 def is_hybrid_property(model, attr_name):
-    return attr_name in get_hybrid_properties(model)
+    names = attr_name.split('.')
+    last_model = model
+    for i in range(len(names)-1):
+        last_model = getattr(last_model, names[i]).property.argument
+    last_name = names[-1]
+    return last_name in get_hybrid_properties(last_model)
 
 
 def is_relationship(attr):
@@ -199,9 +204,3 @@ def is_relationship(attr):
 
 def is_association_proxy(attr):
     return hasattr(attr, 'extension_type') and attr.extension_type == ASSOCIATION_PROXY
-
-
-def get_association_proxy_column_name(attr):
-    # TODO find a better way to get the name
-    name, = [key for key, value in inspect(attr.owning_class).all_orm_descriptors.items() if value is attr]
-    return name

flask_admin/contrib/sqla/view.py
@@ -617,6 +617,7 @@ class ModelView(BaseModelView):
             is_hybrid_property = tools.is_hybrid_property(self.model, name)
             if is_hybrid_property:
                 column = attr
+                column.key = name.split('.')[-1]
             else:
                 columns = tools.get_columns_for_field(attr)
 
@@ -636,7 +637,8 @@ class ModelView(BaseModelView):
                 if not isinstance(name, string_types):
                     visible_name = self.get_column_name(name.property.key)
                 else:
-                    visible_name = self.get_column_name(name)
+                    names = name.split('.')
+                    visible_name = ' / '.join([self.get_column_name(names_item) for names_item in names])
 
             type_name = type(column.type).__name__
 

commit b71634c3f985424370522d75d01cd8257b5e0193

Fix regression with late-evaluation relationships no longer working for filters
Regression was introduced in 47080ae (PR #1348) where `is_hybrid_property` was changed to support remote hybrid properties. When lambdas were encountered in the iteration they were passed as-is to the function `get_hybrid_properties`, which in turn threw the exception `sqlalchemy.exc.NoInspectionAvailable: No inspection system is available for object of type <class 'function'>`.

Test has been updated to ensure that flask-admin will continue to support lambdas as a relationship’s first argument.

flask_admin/contrib/sqla/tools.py
@@ -1,3 +1,5 @@
+import types
+
 from sqlalchemy import tuple_, or_, and_, inspect
 from sqlalchemy.ext.declarative.clsregistry import _class_resolver
 from sqlalchemy.ext.hybrid import hybrid_property
@@ -201,6 +203,8 @@ def is_hybrid_property(model, attr_name):
             last_model = attr.property.argument
             if isinstance(last_model, _class_resolver):
                 last_model = model._decl_class_registry[last_model.arg]
+            elif isinstance(last_model, types.FunctionType):
+                last_model = last_model()
         last_name = names[-1]
         return last_name in get_hybrid_properties(last_model)
     else:

flask_admin/tests/sqla/test_basic.py
@@ -84,7 +84,7 @@ def create_models(db):
 
         # Relation
         model1_id = db.Column(db.Integer, db.ForeignKey(Model1.id))
-        model1 = db.relationship(Model1, backref='model2')
+        model1 = db.relationship(lambda: Model1, backref='model2')
 
     db.create_all()
 