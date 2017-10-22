d006d94e78080a001dc63063f962f81f114bb3c6
Brian Cappello <briancappello@gmail.com>
2017-10-22 15:37:30 -0400

    fix x-editable booleans on list views

--- flask_admin/model/widgets.py
+++ flask_admin/model/widgets.py
@@ -105,6 +105,7 @@ class XEditableWidget(object):
             kwargs['data-rows'] = '5'
         elif field.type == 'BooleanField':
             kwargs['data-type'] = 'select'
+            kwargs['data-value'] = '1' if field.data else ''
             # data-source = dropdown options
             kwargs['data-source'] = json.dumps([
                 {'value': '', 'text': gettext('No')},
