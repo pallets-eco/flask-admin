55234137a2d132bb24a3c7ebe0047c1fb2445eca
david reed <dreed10@gmail.com>
2019-08-13 13:48:36 -0400

    fixes issue https://github.com/flask-admin/flask-admin/issues/1897

--- examples/sqla/admin/__init__.py
+++ examples/sqla/admin/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_babelex import Babel
-
+import os
+import os.path as op
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+
+# Build a sample db on the fly, if one does not exist yet.
+app_dir = op.join(op.realpath(os.path.dirname(__file__)), 'admin')
+database_path = op.join(app_dir, app.config['DATABASE_FILE'])
+if not os.path.exists(database_path):
+    from admin.data import build_sample_db
+    build_sample_db()
 
 # Initialize babel
 babel = Babel(app)
