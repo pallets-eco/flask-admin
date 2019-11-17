b4d308f70d6c2739fe1a3853009d2327da34fb7a
iCaiCai <icaicai@gmail.com>
2019-11-17 17:27:02 +0800

    fix modal window

--- flask_admin/static/admin/js/bs4_modal.js
+++ flask_admin/static/admin/js/bs4_modal.js
@@ -2,3 +2,8 @@
 $('body').on('click.modal.data-api', '[data-toggle="modal"]', function () {
     $($(this).data("target") + ' .modal-content').load($(this).attr('href'));
 });
+
+$(function() {
+  // Apply flask-admin form styles after the modal is loaded
+  window.faForm.applyGlobalStyles(document);
+});
