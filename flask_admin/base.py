330ad34bd7de23b59f2a30c1cfb5dcf7b78ee717
Will Croft <will@croft.io>
2020-07-27 12:34:51 +0200

    Add ability to pass subdomain argument to app factory constructor

--- flask_admin/base.py
+++ flask_admin/base.py
@@ -674,7 +674,7 @@ class Admin(object):
         return self._menu_categories.get(name)
 
     def init_app(self, app, index_view=None,
-                 endpoint=None, url=None):
+                 endpoint=None, url=None, subdomain=None):
         """
             Register all views with the Flask application.
 
@@ -682,6 +682,7 @@ class Admin(object):
                 Flask application instance
         """
         self.app = app
+        self.subdomain = subdomain
 
         self._init_extension()
 
