b5a3c7a6813be7b9063ad2bd97b7ac5df2da47d8
Michael Bukachi <michaelbukachi@gmail.com>
2021-10-27 02:48:26 +0300

    refactor: Update postgres test credentials

--- flask_admin/tests/geoa/__init__.py
+++ flask_admin/tests/geoa/__init__.py
@@ -7,7 +7,7 @@ def setup():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1'
     app.config['CSRF_ENABLED'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flask_admin_test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flask_admin_test'
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- flask_admin/tests/sqla/__init__.py
+++ flask_admin/tests/sqla/__init__.py
@@ -21,7 +21,7 @@ def setup_postgres():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1'
     app.config['CSRF_ENABLED'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flask_admin_test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flask_admin_test'
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
