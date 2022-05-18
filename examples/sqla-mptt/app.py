fd159cfe485120f406e68b385d5eaec26ee39bed
James Addison <jay@jp-hosting.net>
2022-05-18 16:10:41 +0100

    Initial prototype using jquery-treetable (without any tree node drag-and-drop edit persistence)

---
+++ examples/sqla-mptt/app.py
@@ -0,0 +1,52 @@
+import flask_admin
+from flask import Flask, render_template
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_mptt.mixins import BaseNestedSets
+
+# Create application
+app = Flask(__name__)
+
+# Create in-memory database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.sqlite"
+db = SQLAlchemy(app)
+
+# Create models
+class Node(db.Model, BaseNestedSets):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(64))
+
+
+# Administrative class
+class NodeAdmin(ModelView):
+
+    list_template = "admin/treelist.html"
+
+    column_exclude_list = ["tree_id", "left", "right", "level"]
+    form_excluded_columns = ["tree_id", "children", "left", "right", "level"]
+
+    column_formatters = dict(parent=lambda _, __, m, ___: m.parent_id)
+
+    def __init__(self):
+        super(NodeAdmin, self).__init__(Node, db.session, name="Nodes")
+
+
+# Simple page to show images
+@app.route("/")
+def index():
+    nodes = Node.get_tree(session=db.session)
+    return render_template("nodes.html", nodes=nodes)
+
+
+if __name__ == "__main__":
+    # Create admin
+    admin = flask_admin.Admin(app, name="Example: Tree of Models")
+
+    # Add views
+    admin.add_view(NodeAdmin())
+
+    # Create DB
+    db.create_all()
+
+    # Start app
+    app.run(debug=True)
