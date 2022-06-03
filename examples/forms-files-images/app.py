8afe8aabb0f0f40499ab20486b255d26b715114b
Sean McCarthy <sean.mccarthy@hotmail.com>
2022-06-03 08:05:35 -0600

    added example of Bootstrap container, row, and col

--- examples/forms-files-images/app.py
+++ examples/forms-files-images/app.py
@@ -192,7 +192,10 @@ class UserView(sqla.ModelView):
                             rules=["email"],
                             classes="col-xs-6"
                         ),
-                        "phone"
+                        rules.BSCol(
+                            rules=["phone"],
+                            classes="col-xs-6"
+                        )
                     ],
                     classes="justify-content-center"
                 )
