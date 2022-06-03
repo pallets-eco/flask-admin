316faf73001be5a4ff044d4a54674b1a62200fbc
Sean McCarthy <sean.mccarthy@hotmail.com>
2022-06-03 11:59:32 -0600

    improved documentation for legacy class Row() in form rules

--- flask_admin/form/rules.py
+++ flask_admin/form/rules.py
@@ -360,13 +360,39 @@ class FieldSet(NestedRule):
 
 
 class Row(NestedRule):
-    def __init__(self, *columns, **kw):
-        super(Row, self).__init__()
-        self.row_classes = kw.get("row_classes", "form-row")
-        self.col_classes = kw.get("col_classes", "col")
-        self.rules = columns
+    """
+        Bootstrap grid "row" div with automatic Bootstrap columns
+    """
+    def __init__(self, columns=[], separator="", row_classes="form-row", col_classes="col"):
+        """
+            Constructor
+
+            :param columns:
+                Child rule list of column names.
+            :param separator:
+                Default separator between rules when rendering them.
+            :param row_classes:
+                Space-separated classes to use for the Bootstrap row (e.g. "form-row justify-content-center").
+                Default "form-row"
+            :param col_classes:
+                Space-separated classes to use for the Bootstrap columns (e.g. "col-md-6").
+                Default "col"
+        """
+        super(Row, self).__init__(rules=columns, separator=separator)
+        self.row_classes = row_classes
+        self.col_classes = col_classes
 
     def __call__(self, form, form_opts=None, field_args={}):
+        """
+        Render all children when called in the Jinja template.
+
+        :param form:
+            Form object
+        :param form_opts:
+            Form options
+        :param field_args:
+            Optional arguments that should be passed to template or the field
+        """
         cols = []
         for col in self.rules:
             if col.visible_fields:
@@ -374,7 +400,7 @@ class Row(NestedRule):
                 w_args.setdefault("column_class", self.col_classes)
             cols.append(col(form, form_opts, field_args))
 
-        return Markup('<div class="%s">%s</div>' % (self.row_classes, "".join(cols)))
+        return Markup('<div class="form-row">%s</div>' % (self.row_classes, "".join(cols)))
 
 
 class NestedRuleClasses(NestedRule):
