45111b2cecb408c3e54aaa8e15f25c500f252933
Sean McCarthy <sean.mccarthy@hotmail.com>
2022-07-11 13:28:42 -0600

    trying to make it so line endings are always Linux LF instead of CRLF Windows

--- scripts/poetry_publish_package_prod.sh
+++ scripts/poetry_publish_package_prod.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# set -e
-
 # Set the current working directory to the directory in which the script is located, for CI/CD
 cd "$(dirname "$0")"
 echo "Current working directory: $(pwd)"
 
-# Load environment variables from dotenv / .env file in Bash, and remove comments
-export $(cat ../.env | sed 's/#.*//g' | xargs)
-echo "PYPI_TOKEN_PROD: $PYPI_TOKEN_PROD"
+sudo dos2unix ./dos2unix_poetry_publish_package_prod.sh
 
-# First build the files to be uploaded
-poetry build
+bash ./dos2unix_poetry_publish_package_prod.sh
 
-# Publish to the production repository
-# https://python-poetry.org/docs/libraries/
-# https://python-poetry.org/docs/cli/#publish
-# poetry config pypi-token.pypi $PYPI_TOKEN_PROD
-# poetry publish --build 
-# poetry publish --build --username $PYPI_USERNAME_PROD --password $PYPI_PASSWORD_PROD
-poetry publish --username __token__ --password $PYPI_TOKEN_PROD
+exit 0
