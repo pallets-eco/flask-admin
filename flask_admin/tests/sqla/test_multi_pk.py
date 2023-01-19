608fb32b156cacb1ecf5826a9b7f5143e502721b
David Gilman <davidgilman1@gmail.com>
2023-01-18 20:01:16 -0500

    Fix Flask-SQLAlchemy import path

--- flask_admin/tests/sqla/test_multi_pk.py
+++ flask_admin/tests/sqla/test_multi_pk.py
@@ -1,7 +1,7 @@
 from . import setup
 from .test_basic import CustomModelView
 
-from flask_sqlalchemy import Model
+from flask_sqlalchemy.model import Model
 from sqlalchemy.ext.declarative import declarative_base
 
 
