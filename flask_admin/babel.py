bf4b874820f0a887ca85c33d441f6b9a504fb84d
Chris Mayo <aklhfex@gmail.com>
2023-02-01 19:31:15 +0000

    Fix exceptions when flask-babel is installed but not used
    flask-babel's gettext() expects a Babel object to have been created.
    
    Otherwise an exception is raised. For an example see
    tests/test_form_upload.py

--- flask_admin/babel.py
+++ flask_admin/babel.py
@@ -1,10 +1,17 @@
+from flask import current_app, has_app_context
+
 try:
     try:
         from flask_babelex import Domain
     except ImportError:
         from flask_babel import Domain
 
-except ImportError:
+    if has_app_context():
+        current_app().extensions['babel']
+    else:
+        raise KeyError
+
+except (KeyError, ImportError):
     def gettext(string, **variables):
         return string % variables
 
