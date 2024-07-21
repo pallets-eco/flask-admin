pallets-eco/flask-admin

commit 15e36393b7b084522c91732a6b169c34ba5a8d0b

Prevent duplicate requests for bs4_modal.js when using modals (#2407)

flask_admin/templates/bootstrap4/admin/model/modals/create.html
@@ -30,7 +30,3 @@
 
 
 {% endblock %}
-
-{% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs4_modal.js', v='1.0.0') }}"></script>
-{% endblock %}

flask_admin/templates/bootstrap4/admin/model/modals/details.html
@@ -36,5 +36,4 @@
 
 {% block tail %}
   <script src="{{ admin_static.url(filename='admin/js/details_filter.js', v='1.0.0') }}"></script>
-  <script src="{{ admin_static.url(filename='admin/js/bs4_modal.js', v='1.0.0') }}"></script>
 {% endblock %}

flask_admin/templates/bootstrap4/admin/model/modals/edit.html
@@ -25,7 +25,3 @@
   {% endcall %}
 
 {% endblock %}
-
-{% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs4_modal.js', v='1.0.0') }}"></script>
-{% endblock %}

commit 7f35a647303c4ce46213292e8abb8132ab27f1c7

Fix JS elements regression in modal windows
In #2407 we removed some seemingly-duplicate network requests for
`bs4_modal.js` every time a modal window is loaded.

This would have been fine if that file didn't not include any
side-effects, but it did: it re-applied 'global styling' every time the
JS file is loaded.

The goal here was to process 'styling' for any dynamica/interactive
elements, such as map widgets, and fields that use 'select2' or
'x-editable'.

When we removed these 'extra' JS calls, the modal windows stopped
getting these interactive elements enabled/setup.

This patch restores that functionality, while still retaining the goal
of PR #2407: to prevent duplicate network requests for a file.

We load `bs<x>_modal.js` on all pages that can display modals; this file
now only sets up the event listener that is responsible for making sure
a modal is cleared out/refreshed between multiple modal displays.

The logic for applying styling to the window is now added inline to all
of the relevant modal components.

A further improvement on what we had before is that now, the call to add
styling is scoped specifically to the modal. This prevents the issue
described in #2351, where map elements may show up multiple times on
the page because `applyGlobalStyles` is called multiple times.

flask_admin/static/admin/js/bs2_modal.js
@@ -2,8 +2,3 @@
 $('.modal').on('hidden', function() {
   $(this).removeData('modal');
 });
-
-$(function() {
-  // Apply flask-admin form styles after the modal is loaded
-  window.faForm.applyGlobalStyles(document);
-});

flask_admin/static/admin/js/bs3_modal.js
@@ -2,8 +2,3 @@
 $('body').on('hidden.bs.modal', '.modal', function () {
   $(this).removeData('bs.modal').find(".modal-content").empty();
 });
-
-$(function() {
-  // Apply flask-admin form styles after the modal is loaded
-  window.faForm.applyGlobalStyles(document);
-});

flask_admin/static/admin/js/bs4_modal.js
@@ -2,8 +2,3 @@
 $('body').on('click.modal.data-api', '[data-toggle="modal"]', function () {
     $($(this).data("target") + ' .modal-content').load($(this).attr('href'));
 });
-
-$(function() {
-  // Apply flask-admin form styles after the modal is loaded
-  window.faForm.applyGlobalStyles(document);
-});

flask_admin/templates/bootstrap2/admin/file/list.html
@@ -193,4 +193,5 @@
     {{ actionslib.script(_gettext('Please select at least one file.'),
                          actions,
                          actions_confirmation) }}
+    <script src="{{ admin_static.url(filename='admin/js/bs2_modal.js', v='1.0.0') }}"></script>
 {% endblock %}

flask_admin/templates/bootstrap2/admin/model/list.html
@@ -189,6 +189,7 @@
 
     {{ lib.form_js() }}
     <script src="{{ admin_static.url(filename='admin/js/filters.js', v='1.0.0') }}"></script>
+    <script src="{{ admin_static.url(filename='admin/js/bs2_modal.js', v='1.0.0') }}"></script>
 
     {{ actionlib.script(_gettext('Please select at least one record.'),
                         actions,

flask_admin/templates/bootstrap2/admin/model/modals/create.html
@@ -14,7 +14,7 @@
 {% endblock %}
 
 {% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs2_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal'))</script>
 
   <script>
   // fill the header of modal dynamically

flask_admin/templates/bootstrap2/admin/model/modals/details.html
@@ -29,7 +29,7 @@
 
 {% block tail %}
   <script src="{{ admin_static.url(filename='admin/js/details_filter.js', v='1.0.0') }}"></script>
-  <script src="{{ admin_static.url(filename='admin/js/bs2_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal'))</script>
 
   <script>
   // fill the header of modal dynamically

flask_admin/templates/bootstrap2/admin/model/modals/edit.html
@@ -14,7 +14,7 @@
 {% endblock %}
 
 {% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs2_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal'))</script>
 
   <script>
   // fill the header of modal dynamically

flask_admin/templates/bootstrap3/admin/file/list.html
@@ -193,4 +193,5 @@
     {{ actionslib.script(_gettext('Please select at least one file.'),
                          actions,
                          actions_confirmation) }}
+    <script src="{{ admin_static.url(filename='admin/js/bs3_modal.js', v='1.0.0') }}"></script>
 {% endblock %}

flask_admin/templates/bootstrap3/admin/model/list.html
@@ -190,6 +190,7 @@
 
     {{ lib.form_js() }}
     <script src="{{ admin_static.url(filename='admin/js/filters.js', v='1.0.0') }}"></script>
+    <script src="{{ admin_static.url(filename='admin/js/bs3_modal.js', v='1.0.0') }}"></script>
 
     {{ actionlib.script(_gettext('Please select at least one record.'),
                         actions,

flask_admin/templates/bootstrap3/admin/model/modals/create.html
@@ -20,5 +20,5 @@
 {% endblock %}
 
 {% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs3_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
 {% endblock %}

flask_admin/templates/bootstrap3/admin/model/modals/details.html
@@ -36,5 +36,5 @@
 
 {% block tail %}
   <script src="{{ admin_static.url(filename='admin/js/details_filter.js', v='1.0.0') }}"></script>
-  <script src="{{ admin_static.url(filename='admin/js/bs3_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
 {% endblock %}

flask_admin/templates/bootstrap3/admin/model/modals/edit.html
@@ -22,5 +22,5 @@
 {% endblock %}
 
 {% block tail %}
-  <script src="{{ admin_static.url(filename='admin/js/bs3_modal.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
 {% endblock %}

flask_admin/templates/bootstrap4/admin/model/modals/create.html
@@ -30,3 +30,7 @@
 
 
 {% endblock %}
+
+{% block tail %}
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
+{% endblock %}

flask_admin/templates/bootstrap4/admin/model/modals/details.html
@@ -36,4 +36,5 @@
 
 {% block tail %}
   <script src="{{ admin_static.url(filename='admin/js/details_filter.js', v='1.0.0') }}"></script>
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
 {% endblock %}

flask_admin/templates/bootstrap4/admin/model/modals/edit.html
@@ -23,5 +23,8 @@
         {{ lib.render_form_buttons(return_url, extra=None, is_modal=True) }}
   </div>
   {% endcall %}
+{% endblock %}
 
+{% block tail %}
+  <script>window.faForm.applyGlobalStyles(document.getElementsByClassName('modal-content'));</script>
 {% endblock %}