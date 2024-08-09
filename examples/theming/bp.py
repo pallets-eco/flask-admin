2a35f64ad83231411be970d67a10694360b951c5
PrinceWebCoder <bekjanov2310@gmail.com>
2024-08-09 18:02:45 +0500

    Example theming: added bp.py

---
+++ examples/theming/bp.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask import render_template_string
+
+
+bp = Blueprint('main', __name__, template_folder='my_admin/templates')
+
+
+@bp.route('/')
+def index():
+    return render_template_string('''
+        <h1>Hello, World!</h1>
+        <ul>
+            <li>
+                <a href="{{ url_for('basicadmin.index') }}">
+                    Basic Admin Panel
+                </a>
+            </li>
+            <li>
+                <a href="{{ url_for('superadmin.index') }}">
+                    Super Admin Panel
+                </a>
+            </li>
+        </ul>
+    ''')
