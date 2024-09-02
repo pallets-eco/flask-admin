314eb02c6904bee2886fadd983a5a8216b9132de
Chris Mayo <aklhfex@gmail.com>
2024-09-02 19:31:50 +0100

    Update documentation copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'flask-admin'
-copyright = u'2012-2019, Flask-Admin Team'
+copyright = u'2012-2024, Flask-Admin Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
