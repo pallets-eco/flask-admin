3a0f17842c539380b64154d1cf8f2a43c028cf23
Chris Mayo <aklhfex@gmail.com>
2024-10-02 19:23:49 +0100

    Publish constructor docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -90,6 +90,11 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 
+# -- sphinx.ext.autodoc Configuration ------------------------------------------
+
+autoclass_content = 'both'
+
+
 # -- Options for HTML output ---------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
