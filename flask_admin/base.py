404d8287115ab30e8e81daedd1a93ebc6cd0d2ca
Chris Mayo <aklhfex@gmail.com>
2024-10-03 19:26:40 +0100

    Allow form.widgets.RenderTemplateWidget to use the theme object

--- flask_admin/base.py
+++ flask_admin/base.py
@@ -548,6 +548,8 @@ class Admin(object):
         # Register with application
         if app is not None:
             self._init_extension()
+            # Make theme available via current_app
+            app.config['theme'] = self.theme
 
     def _validate_admin_host_and_subdomain(self):
         if self.subdomain is not None and self.host is not None:
