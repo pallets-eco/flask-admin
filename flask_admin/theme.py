b1df7e64a85e59be2c37d263ed752442f7244a03
Samuel Williams <samuelhwilliams@gmail.com>
2024-10-11 20:57:08 +0100

    Change from inheritance to protocols
    This makes it possible for themes to include behaviour, eg so that you
    could do `Admin(theme=MyCustomTheme(app))` to have it do something with
    the flask app, like register routes for serving static assets etc,
    without needing to do that with a separate helper, eg
    
    ```
    admin = Admin(theme=MyCustomTheme())
    MyCustomThemeHelper(app)
    ```

--- flask_admin/theme.py
+++ flask_admin/theme.py
@@ -1,17 +1,21 @@
-import typing
+import typing as t
 from dataclasses import dataclass
 from functools import partial
 
 
-@dataclass
-class Theme:
+class Theme(t.Protocol):
     folder: str  # The templates folder name to use
     base_template: str
 
 
+class Bootstrap(t.Protocol):
+    swatch: str = "default"
+    fluid: bool = False
+
+
 @dataclass
-class BootstrapTheme(Theme):
-    folder: typing.Literal["bootstrap4"]
+class BootstrapTheme(Theme, Bootstrap):
+    folder: t.Literal["bootstrap4"]
     base_template: str = "admin/base.html"
     swatch: str = "default"
     fluid: bool = False
