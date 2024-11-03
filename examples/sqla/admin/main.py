c7c1aac0ba59a674e0f74e3f7bbf2bc5346b1331
ElLorans <lorenzo.cerreta@gmail.com>
2024-11-03 21:54:08 +0100

    use redirect in place of send_file

--- examples/sqla/admin/main.py
+++ examples/sqla/admin/main.py
@@ -1,5 +1,6 @@
 import flask_admin as admin
-from flask import send_file
+from flask import redirect
+from flask import url_for
 from flask_admin.babel import gettext
 from flask_admin.base import MenuLink
 from flask_admin.contrib import sqla
@@ -42,7 +43,7 @@ def index():
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_file("static/favicon.ico")
+    return redirect(url_for("static", filename="/favicon.ico"))
 
 
 # Custom filter class
