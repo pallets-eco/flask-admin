9bab80faefb22079b525487ee4911e53cbad2c15
hasansezertasan <hasansezertasan@gmail.com>
2025-06-27 00:36:18 +0300

    chore: remove config.py

--- examples/auth/main.py
+++ examples/auth/main.py
@@ -19,7 +19,27 @@ from flask_security.utils import hash_password
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+# Create dummy secret key so we can use sessions
+app.config["SECRET_KEY"] = "123456790"
+# Create in-memory database
+app.config["DATABASE_FILE"] = "db.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + app.config["DATABASE_FILE"]
+app.config["SQLALCHEMY_ECHO"] = True
+# Flask-Security config
+app.config["SECURITY_URL_PREFIX"] = "/admin"
+app.config["SECURITY_PASSWORD_HASH"] = "pbkdf2_sha512"
+app.config["SECURITY_PASSWORD_SALT"] = "ATGUOHAELKiubahiughaerGOJAEGj"
+# Flask-Security URLs, overridden because they don't put a / at the end
+app.config["SECURITY_LOGIN_URL"] = "/login/"
+app.config["SECURITY_LOGOUT_URL"] = "/logout/"
+app.config["SECURITY_REGISTER_URL"] = "/register/"
+app.config["SECURITY_POST_LOGIN_VIEW"] = "/admin/"
+app.config["SECURITY_POST_LOGOUT_VIEW"] = "/admin/"
+app.config["SECURITY_POST_REGISTER_VIEW"] = "/admin/"
+# Flask-Security features
+app.config["SECURITY_REGISTERABLE"] = True
+app.config["SECURITY_SEND_REGISTER_EMAIL"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 admin = Admin(
     app,
