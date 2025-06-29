94a9ccc21915bead832302f536922ff3436ed795
Samuel Williams <samuelhwilliams@gmail.com>
2025-06-29 17:24:29 +0100

    Update makefile/babel script to use uv

--- babel/babel.sh
+++ babel/babel.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-pybabel extract -F babel.ini -k _gettext -k _ngettext -k lazy_gettext -o admin.pot --project Flask-Admin ../flask_admin
+uv run pybabel extract -F babel.ini -k _gettext -k _ngettext -k lazy_gettext -o admin.pot --project Flask-Admin ../flask_admin
 
 if [ "$1" = '--update' ]; then
-    pybabel update -i admin.pot -d ../flask_admin/translations -D admin -N
+    uv run pybabel update -i admin.pot -d ../flask_admin/translations -D admin -N
 fi
 
-pybabel compile -f -D admin -d ../flask_admin/translations/
+uv run pybabel compile -f -D admin -d ../flask_admin/translations/
 
 # docs
 cd ..
 make gettext
 cp build/locale/*.pot babel/
-sphinx-intl update -p build/locale/ -d flask_admin/translations/
+uv run sphinx-intl update -p build/locale/ -d flask_admin/translations/
