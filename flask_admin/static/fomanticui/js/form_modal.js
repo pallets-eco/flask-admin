e2ab9b41c612312785d76c5833b351442d1a75f4
PrinceWebCoder <bekjanov2310@gmail.com>
2025-08-01 23:45:03 +0500

    Show loading state during modal form request

--- flask_admin/static/fomanticui/js/form_modal.js
+++ flask_admin/static/fomanticui/js/form_modal.js
@@ -4,15 +4,27 @@ $(document).ready(function () {
         observeChanges: true,
         onApprove: function (elm) {
             $('#fa-modal-content form').submit();
+        },
+        onHidden: function () {
+            // Revert loading dimmer for better performance and UX
+            $('#fa-modal-content').html('<br><br><div class="ui active inverted dimmer"><div class="ui loader"></div></div><br><br>');
         }
     });
     $('a.modal-opener').click(function (event) {
         event.preventDefault();
-        var url = $(this).attr('href');
-        $('#fa-modal-content').empty();
         $('#fa-modal').modal('show');
-        // @TODO: Dimmer a loader while fetching
-        $('#fa-modal-content').load(url);
-        window.faForm.applyGlobalStyles(document.getElementById('fa-modal-content'));
+        var $this = $(this);
+        var action = $this.data('action');
+        if (action === 'view') {
+            $('#fa-modal > .actions').hide();
+        } else if (action === 'edit') {
+            $('#fa-modal > .actions').show();
+        }
+        var url = $this.attr('href');
+        $('#fa-modal-content').load(url, function () {
+            // Remove loading dimmer after content is loaded
+            window.faForm.applyGlobalStyles(document.getElementById('fa-modal-content'));
+        });
+
     });
 });
