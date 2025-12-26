79842adf2ec7971e792601a7e1a2c1a43aecbe57
Sami Alfattani <Sami_alfattani@hotmail.com>
2025-12-26 18:46:54 +0300

    add delete button on details and edit views
    Fixes #1327
    
    fix showing confirm message on Modal
    
    Align "Delete" button with other button groups in Edit page
    
    fix

--- flask_admin/tests/sqla/test_basic.py
+++ flask_admin/tests/sqla/test_basic.py
@@ -3826,3 +3826,76 @@ def test_sqlalite_session_raises(app: Flask, sqla_db_ext: T_ANY_SQLA_PROVIDER) -
         sqla_db_ext.create_all()
 
     ModelView(Model, sqla_db_ext.db.session)  # type: ignore[arg-type]
+
+
+@pytest.mark.parametrize(
+    "with_delete, modal", [(True, True), (True, False), (False, True), (False, False)]
+)
+def test_del_btn_in_edit_and_details(
+    app: Flask,
+    sqla_db_ext: T_ANY_SQLA_PROVIDER,
+    admin: Admin,
+    session_or_db: T_LITERAL_SESSION_OR_DB,
+    with_delete: bool,
+    modal: bool,
+) -> None:
+    
+
+    with app.app_context():
+        Model1, Model2 = create_models(sqla_db_ext)
+        param = skip_or_return_session_or_db(sqla_db_ext, session_or_db)
+        sqla_db_ext.db.session.add_all(
+            [
+                Model1(test1="record-1"),
+                Model1(test1="record-2"),
+            ]
+        )
+        sqla_db_ext.db.session.commit()
+
+        class MyModelView(CustomModelView):
+            can_edit = True
+            can_view_details = True
+
+            can_delete = with_delete
+            edit_modal = modal
+            details_modal = modal
+
+        # test column_list with a list of strings
+        view = MyModelView(
+            Model1,
+            param,
+            name="Without Modal",
+        )
+        admin.add_view(view)
+
+        client = app.test_client()
+
+        rv = client.get("/admin/model1/details/?id=2")
+        data = rv.data.decode("utf-8")
+        if with_delete:
+            assert "btn-delete" in data
+        else:
+            assert "btn-delete" not in data
+
+        rv = client.get("/admin/model1/edit/?id=2")
+        data = rv.data.decode("utf-8")
+        if with_delete:
+            assert "btn-delete" in data
+        else:
+            assert "btn-delete" not in data
+
+
+@pytest.mark.xfail(
+    reason="SQLALiteProvider does not support passing db.session directly",
+    raises=TypeError,
+    strict=True,
+)
+def test_sqlalite_session_raises(app, sqla_db_ext):
+    if sqla_db_ext.__class__.__name__ != "SQLALiteProvider":
+        pytest.skip("Only relevant for SQLALiteProvider")
+
+    Model = create_models(sqla_db_ext)
+    with app.app_context():
+        sqla_db_ext.create_all()
+
+    ModelView(Model, sqla_db_ext.db.session)
