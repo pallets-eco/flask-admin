cdbbb2e6418ff2ecae095c660d81dd8913cff85d
ElLorans <lorenzo.cerreta@gmail.com>
2026-01-19 00:19:34 +0100

    test flask-sqla-lite, too

--- flask_admin/tests/bootstrap4/test_renders.py
+++ flask_admin/tests/bootstrap4/test_renders.py
@@ -7,67 +7,53 @@ from sqlalchemy.orm import backref
 from sqlalchemy.orm import relationship
 
 
-def create_models(db):
-    class User(db.Model):  # type: ignore[name-defined, misc]
-        def __init__(
-            self,
-            first_name=None,
-            last_name=None,
-        ):
-            self.first_name = first_name
-            self.last_name = last_name
-
+def create_models(provider):
+    class User(provider.Base):  # type: ignore[name-defined, misc]
+        __tablename__ = "user"
         id = Column(Integer, primary_key=True)
         first_name = Column(String(20))
         last_name = Column(String(20))
 
-        def __unicode__(self):
-            return self.first_name
-
         def __str__(self):
             return self.first_name
 
-    class Post(db.Model):  # type: ignore[name-defined, misc]
-        def __init__(self, title=None, desc=None, author=None):
-            self.title = title
-            self.desc = desc
-            self.author = author
-
+    class Post(provider.Base):  # type: ignore[name-defined, misc]
+        __tablename__ = "post"
         id = Column(Integer, primary_key=True)
         title = Column(String(20))
         desc = Column(String(20))
         author_id = Column(Integer, ForeignKey("user.id"))
         author = relationship("User", backref=backref("posts", lazy="dynamic"))
 
-    db.create_all()
+    provider.create_all()
 
     return User, Post
 
 
-def fill_data(db, User, Post):
-    u1 = User("user1", "userdesc1")
-    u2 = User("user2", "userdesc2")
+def fill_data(provider, User, Post):
+    u1 = User(first_name="user1", last_name="userdesc1")
+    u2 = User(first_name="user2", last_name="userdesc2")
 
-    db.session.add_all(
+    provider.db.session.add_all(
         [
             u1,
             u2,
-            Post("post1", "postdesc1", u1),
-            Post("post2", "postdesc2", u2),
+            Post(title="post1", desc="postdesc1", author=u1),
+            Post(title="post2", desc="postdesc2", author=u2),
         ]
     )
-    db.session.commit()
+    provider.db.session.commit()
 
 
-def test_css_class_with_combined(app, db, admin):
+def test_css_class_with_combined(app, provider, admin):
     with app.app_context():
-        User, Post = create_models(db)
-        fill_data(db, User, Post)
+        User, Post = create_models(provider)
+        fill_data(provider, User, Post)
 
         class PostView(ModelView):
             column_list = ["title", "author.first_name"]
 
-        admin.add_view(PostView(Post, db.session))
+        admin.add_view(PostView(Post, provider.db))
 
         client = app.test_client()
         response = client.get("/admin/post/")
