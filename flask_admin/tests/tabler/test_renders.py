014999ac848b19afb8513651415ddf668c08ecc9
ElLorans <lorenzo.cerreta@gmail.com>
2026-05-23 10:31:26 +0200

    fix types

--- flask_admin/tests/tabler/test_renders.py
+++ flask_admin/tests/tabler/test_renders.py
@@ -1,4 +1,7 @@
+import typing as t
+
 import pytest
+from flask import Flask
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
@@ -8,18 +11,19 @@ from sqlalchemy.orm import relationship
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla.view import ModelView
+from flask_admin.tests.conftest import T_ANY_SQLA_PROVIDER
 from flask_admin.theme import TablerTheme
 
 
-def create_models(sqla_db_ext):
+def create_models(sqla_db_ext: T_ANY_SQLA_PROVIDER) -> tuple[type, type]:
     class User(sqla_db_ext.Base):  # type: ignore[name-defined, misc]
         __tablename__ = "user"
         id = Column(Integer, primary_key=True)
         first_name = Column(String(20))
         last_name = Column(String(20))
 
-        def __str__(self):
-            return self.first_name
+        def __str__(self) -> str:
+            return self.first_name  # type: ignore[return-value]
 
     class Post(sqla_db_ext.Base):  # type: ignore[name-defined, misc]
         __tablename__ = "post"
@@ -31,10 +35,6 @@ def create_models(sqla_db_ext):
 
     sqla_db_ext.create_all()
 
-    return User, Post
-
-
-def fill_data(sqla_db_ext, User, Post):
     u1 = User(first_name="user1", last_name="userdesc1")
     u2 = User(first_name="user2", last_name="userdesc2")
 
@@ -47,6 +47,8 @@ def fill_data(sqla_db_ext, User, Post):
         ]
     )
     sqla_db_ext.db.session.commit()
+
+    return User, Post
 
 
 @pytest.mark.parametrize(
@@ -70,12 +72,17 @@ def fill_data(sqla_db_ext, User, Post):
         ),
     ],
 )
-def test_layout_renders(app, babel, sqla_db_ext, layout, markers):
+def test_layout_renders(
+    app: Flask,
+    babel: object | None,
+    sqla_db_ext: T_ANY_SQLA_PROVIDER,
+    layout: t.Literal["vertical", "fluid", "condensed"],
+    markers: list[str],
+) -> None:
     admin = Admin(app, theme=TablerTheme(layout=layout))
 
     with app.app_context():
         User, Post = create_models(sqla_db_ext)
-        fill_data(sqla_db_ext, User, Post)
 
         class PostView(ModelView):
             column_list = ["title", "author.first_name"]
